7639fd0a9e55b0eae4e604a1d2b64d9472582fb6
Dmitry Esyunin <74874618+dmitry-esyunin@users.noreply.github.com>
2022-01-23 15:01:59 +0300

    Выполнил задание 2. Добавил кнопку перехода с первого экрана на второй

--- app/src/main/java/com/gb1919/hw01/MainActivity.java
+++ app/src/main/java/com/gb1919/hw01/MainActivity.java
@@ -3,6 +3,9 @@ package com.gb1919.hw01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_second);
+
+                Button button_back = findViewById(R.id.btn_back);
+                button_back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setContentView(R.layout.activity_main);
+                    }
+                });
+            }
+        });
+
+
+
     }
+
+
+
 }
